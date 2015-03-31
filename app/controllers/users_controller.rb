e13ea6534db0d58902df96c2fe375abe674d0e7a
Bradford Nemmers <bradfordnemmers@Bradfords-MacBook-Pro.local>
2015-03-30 20:16:15 -0700

    new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,7 +67,7 @@ end
   # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
-      if @user.update(user_params, avatar_file_name)
+      if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         # format.json { render :show, status: :ok, location: @user }
       else
